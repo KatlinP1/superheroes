4db71f17b55437556d919667de35dbcc828acf11
Katlin <email@email.com>
2020-09-09 10:52:33 +0300

    superv

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ let superheroes = require('superheroes');
 //random superkangelase leidmine nimekirast
 console.log(superheroes.random()); 
 
-superheroes.all.forEach(hero=> {
+/*superheroes.all.forEach(hero=> {
     console.log(hero);
-});
+});*/
+
+/*
+TASK 1 
+1. get the name of all supervilliains 
+2. get random sv name
+3. maka a random superhero fight a random supervillian in the console.log()
+*/
+
+// list on superv
+let supervillains=require('supervillains');
+//console.log(supervillains.all);
+
+supervillains.all.forEach(supervillain =>{
+    console.log(supervillain);
+} );
+
+//random nimi 
+console.log(supervillains.random());
+
+//võitlemine 
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
+
